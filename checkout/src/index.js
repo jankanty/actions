95ed384c63845b68b8d08b5a120333b492f248f5
Jan-Kanty Pawelski <jan.kanty.pawelski@gmail.com>
2021-02-21 14:45:40 +0100

    Test

--- checkout/src/index.js
+++ checkout/src/index.js
@@ -4,6 +4,11 @@ const {exec} = require('@actions/exec');
 
 async function run() {
   await exec('echo Running checkout action...')
+
+  const email = await exec('git log -1 --format=\'%ae\'');
+  const name = await exec('git log -1 --format=\'%ae\'');
+
+  console.log(`${ name } <${ email }>`);
 }
 
 
